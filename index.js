f71a066066e7ecd36350282a0a8f3b5e6b44e9d7
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-11-11 15:56:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const PORT = process.env.PORT || 8080;
 const express = require("express");
 const Datastore = require("nedb");
+const PORT = process.env.PORT || 8080;
+
 
 const app = express();
 app.use(express.static("public"));
